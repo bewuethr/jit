78cbb455561ed68cd29c93530e2162141c426edd
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-03-06 00:22:26 -0500

    Find best common ancestor in merging histories
    Instead of relying the history to be a tree, we now can also find the
    best common ancestor in histories with merges. Instead of returning the
    first commit that is reachable from both starting commits, we keep a
    list of such commits, and mark them as stale if they're reachable from
    each other.
    
    We also follow both parents when traversing history, and not just the
    first (and only) parent like before.
    
    This functionality is layered into a new Bases class, which uses
    CommonAncestors internally, but maintains the aforementioned list.
    
    We can't yet handle the situation where there are multiple best common
    ancestors, and return all of them for now.

--- lib/merge/common_ancestors.rb
+++ lib/merge/common_ancestors.rb
@@ -2,37 +2,52 @@ module Merge
   class CommonAncestors
     BOTH_PARENTS = Set.new([:parent1, :parent2])
 
-    def initialize(database, one, two)
+    def initialize(database, one, twos)
       @database = database
       @flags = Hash.new { |hash, oid| hash[oid] = Set.new }
       @queue = []
+      @results = []
 
       insert_by_date(@queue, @database.load(one))
       @flags[one].add(:parent1)
 
-      insert_by_date(@queue, @database.load(two))
-      @flags[two].add(:parent2)
+      twos.each do |two|
+        insert_by_date(@queue, @database.load(two))
+        @flags[two].add(:parent2)
+      end
     end
 
     def find
-      until @queue.empty?
-        commit = @queue.shift
-        flags = @flags[commit.oid]
+      process_queue until all_stale?
+      @results.map(&:oid).reject { |oid| marked?(oid, :stale) }
+    end
 
-        return commit.oid if flags == BOTH_PARENTS
+    def marked?(oid, flag) = @flags[oid].include?(flag)
 
+    private def all_stale?
+      @queue.all? { |commit| marked?(commit.oid, :stale) }
+    end
+
+    private def process_queue
+      commit = @queue.shift
+      flags = @flags[commit.oid]
+
+      if flags == BOTH_PARENTS
+        flags.add(:result)
+        insert_by_date(@results, commit)
+        add_parents(commit, flags + [:stale])
+      else
         add_parents(commit, flags)
       end
     end
 
     private def add_parents(commit, flags)
-      return unless commit.parent
+      commit.parents.each do |parent|
+        next if @flags[parent].superset?(flags)
 
-      parent = @database.load(commit.parent)
-      return if @flags[parent.oid].superset?(flags)
-
-      @flags[parent.oid].merge(flags)
-      insert_by_date(@queue, parent)
+        @flags[parent].merge(flags)
+        insert_by_date(@queue, @database.load(parent))
+      end
     end
 
     private def insert_by_date(list, commit)
--- test/merge/common_ancestors_test.rb
+++ test/merge/common_ancestors_test.rb
@@ -3,6 +3,7 @@ require "fileutils"
 require "pathname"
 
 require "database"
+require "merge/bases"
 require "merge/common_ancestors"
 
 class Merge::TestMergeCommonAncestors < Minitest::Test
@@ -14,11 +15,11 @@ class Merge::TestMergeCommonAncestors < Minitest::Test
 
   def database = @database ||= Database.new(db_path)
 
-  def commit(parent, message)
+  def commit(parents, message)
     @commits ||= {}
     @time ||= Time.now
 
-    parents = [@commits[parent]]
+    parents = parents.map { |oid| @commits[oid] }
     author = Database::Author.new("A. U. Thor", "author@example.com", @time)
     commit = Database::Commit.new(parents, "0" * 40, author, message)
 
@@ -27,12 +28,22 @@ class Merge::TestMergeCommonAncestors < Minitest::Test
   end
 
   def chain(names)
-    names.each_cons(2) { |parent, message| commit(parent, message) }
+    names.each_cons(2) { |parent, message| commit([*parent], message) }
   end
 
   def ancestor(left, right)
-    common = Merge::CommonAncestors.new(database, @commits[left], @commits[right])
-    database.load(common.find).message
+    common = Merge::CommonAncestors.new(database, @commits[left], [@commits[right]])
+    get_result(common)
+  end
+
+  def merge_base(left, right)
+    bases = Merge::Bases.new(database, @commits[left], @commits[right])
+    get_result(bases)
+  end
+
+  def get_result(common)
+    commits = common.find.map { |oid| database.load(oid).message }
+    (commits.size == 1) ? commits.first : commits
   end
 end
 
@@ -105,5 +116,148 @@ class Merge::TestMergeCommonAncestorsForkingHistory < Merge::TestMergeCommonAnce
 
   def test_find_root_commit
     assert_equal("A", ancestor("J", "A"))
+  end
+end
+
+class Merge::TestMergeCommonAncestorsHistorywithMerge < Merge::TestMergeCommonAncestors
+  def setup
+    super
+
+    #   A   B   C   G   H
+    #   o---o---o---o---o
+    #        \     /
+    #         o---o---o
+    #         D   E   F
+    chain([nil] + %w[A B C])
+    chain(%w[B D E F])
+    commit(%w[C E], "G")
+    chain(%w[G H])
+  end
+
+  def test_find_most_recent_common_ancestor
+    assert_equal("E", ancestor("H", "F"))
+  end
+
+  def test_find_common_ancestor_of_merge_and_its_parent
+    assert_equal("C", ancestor("C", "G"))
+    assert_equal("E", ancestor("G", "E"))
+  end
+end
+
+class Merge::TestMergeCommonAncestorsHistorywithMergeFurtherFromOneParent < Merge::TestMergeCommonAncestors
+  def setup
+    super
+
+    #   A   B   C   G   H   J
+    #   o---o---o---o---o---o
+    #        \     /
+    #         o---o---o
+    #         D   E   F
+    chain([nil] + %w[A B C])
+    chain(%w[B D E F])
+    commit(%w[C E], "G")
+    chain(%w[G H J])
+  end
+
+  def test_find_all_common_ancestors
+    assert_equal(%w[E B], ancestor("J", "F"))
+  end
+
+  def test_find_best_common_ancestor
+    assert_equal("E", merge_base("J", "F"))
+  end
+end
+
+class Merge::TestMergeCommonAncestorsCommitsBetweenAncestorAndMerge < Merge::TestMergeCommonAncestors
+  def setup
+    super
+
+    #   A   B   C       H   J
+    #   o---o---o-------o---o
+    #        \         /
+    #         o---o---o G
+    #         D  E \
+    #               o F
+    chain([nil] + %w[A B C])
+    chain(%w[B D E F])
+    chain(%w[E G])
+    commit(%w[C G], "H")
+    chain(%w[H J])
+  end
+
+  def test_find_all_common_ancestors
+    assert_equal(%w[B E], ancestor("J", "F"))
+  end
+
+  def test_find_best_common_ancestor
+    assert_equal("E", merge_base("J", "F"))
+  end
+end
+
+class Merge::TestMergeCommonAncestorsEnoughHistoryToFindAllStale < Merge::TestMergeCommonAncestors
+  def setup
+    super
+
+    #   A   B   C             H   J
+    #   o---o---o-------------o---o
+    #        \      E        /
+    #         o-----o-------o
+    #        D \     \     / G
+    #           \     o   /
+    #            \    F  /
+    #             o-----o
+    #             P     Q
+    chain([nil] + %w[A B C])
+    chain(%w[B D E F])
+    chain(%w[D P Q])
+    commit(%w[E Q], "G")
+    commit(%w[C G], "H")
+    chain(%w[H J])
+  end
+
+  def test_find_best_common_ancestor
+    assert_equal("E", ancestor("J", "F"))
+    assert_equal("E", ancestor("F", "J"))
+  end
+end
+
+class Merge::TestMergeCommonAncestorsManyCommonAncestors < Merge::TestMergeCommonAncestors
+  def setup
+    super
+
+    #         L   M   N   P   Q   R   S   T
+    #         o---o---o---o---o---o---o---o
+    #        /       /       /       /
+    #   o---o---o...o---o...o---o---o---o---o
+    #   A   B  C \  D  E \  F  G \  H   J   K
+    #             \       \       \
+    #              o---o---o---o---o---o
+    #              U   V   W   X   Y   Z
+    chain([nil] + %w[A B C] + (1..4).map { |n| "pad-1-#{n}" } +
+        %w[D E] + (1..4).map { |n| "pad-2-#{n}" } +
+        %w[F G] +
+        %w[H J K])
+
+    chain(%w[B L M])
+    commit(%w[M D], "N")
+    chain(%w[N P])
+    commit(%w[P F], "Q")
+    chain(%w[Q R])
+    commit(%w[R H], "S")
+    chain(%w[S T])
+
+    chain(%w[C U V])
+    commit(%w[V E], "W")
+    chain(%w[W X])
+    commit(%w[X G], "Y")
+    chain(%w[Y Z])
+  end
+
+  def test_find_multiple_candidate_common_ancestors
+    assert_equal(%w[G D B], ancestor("T", "Z"))
+  end
+
+  def test_find_best_common_ancestor
+    assert_equal("G", merge_base("T", "Z"))
   end
 end
