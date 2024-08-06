c542cb1e2ac36e4a6dcb1ac41de9315795d7ca3c
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-08-06 13:58:58 -0700

    Add in-memory representation of config file
    To handle Git config files, this adds a "Config" class with an in-memory
    representation of such a file. The representation tries to preserve
    existing formatting of a file, and provides an interface to create,
    read, and update values.

---
+++ lib/config.rb
@@ -0,0 +1,231 @@
+require_relative "lockfile"
+
+class Config
+  SECTION_LINE = /\A\s*\[([a-z0-9-]+)( "(.+)")?\]\s*(\Z|#|;)/i
+  VARIABLE_LINE = /\A\s*([a-z][a-z0-9-]*)\s*=\s*(.*?)\s*(\Z|#|;)/im
+  BLANK_LINE = /\A\s*(\Z|#|;)/
+  INTEGER = /\A-?[1-9][0-9]*\Z/
+
+  ParseError = Class.new(StandardError)
+  Conflict = Class.new(StandardError)
+
+  Variable = Struct.new(:name, :value) do
+    def self.normalize(name) = name&.downcase
+
+    def self.serialize(name, value) = "\t#{name} = #{value}\n"
+  end
+
+  Section = Struct.new(:name) do
+    def self.normalize(name)
+      return [] if name.empty?
+      [name.first.downcase, name.drop(1).join(".")]
+    end
+
+    def heading_line
+      line = "[#{name.first}"
+      line.concat(%( "#{name.drop(1).join(".")}")) if name.size > 1
+      line.concat("]\n")
+    end
+  end
+
+  Line = Struct.new(:text, :section, :variable) do
+    def normal_variable = Variable.normalize(variable&.name)
+  end
+
+  def initialize(path)
+    @path = path
+    @lockfile = Lockfile.new(path)
+    @lines = nil
+  end
+
+  def open = read_config_file unless @lines
+
+  def open_for_update
+    @lockfile.hold_for_update
+    read_config_file
+  end
+
+  def save
+    @lines.each do |section, lines|
+      lines.each { |line| @lockfile.write(line.text) }
+    end
+    @lockfile.commit
+  end
+
+  def get_all(key)
+    key, var = split_key(key)
+    _, lines = find_lines(key, var)
+
+    lines.map { _1.variable.value }
+  end
+
+  def get(key) = get_all(key).last
+
+  def add(key, value)
+    key, var = split_key(key)
+    section, _ = find_lines(key, var)
+
+    add_variable(section, key, var, value)
+  end
+
+  def set(key, value)
+    key, var = split_key(key)
+    section, lines = find_lines(key, var)
+
+    case lines.size
+    when 0 then add_variable(section, key, var, value)
+    when 1 then update_variable(lines.first, var, value)
+    else
+      message = "cannot overwrite multiple values with a single value"
+      raise Conflict, message
+    end
+  end
+
+  def replace_all(key, value)
+    key, var = split_key(key)
+    section, lines = find_lines(key, var)
+
+    remove_all(section, lines)
+    add_variable(section, key, var, value)
+  end
+
+  def unset_all(key)
+    key, var = split_key(key)
+    section, lines = find_lines(key, var)
+
+    return unless section
+    yield lines if block_given?
+
+    remove_all(section, lines)
+    lines = lines_for(section)
+    remove_section(key) if lines.size == 1
+  end
+
+  def unset(key)
+    unset_all(key) do |lines|
+      raise Conflict, "#{key} has multiple values" if lines.size > 1
+    end
+  end
+
+  def subsections(name)
+    name, _ = Section.normalize([name])
+    sections = []
+
+    @lines.each_key do |main, sub|
+      sections.push(sub) if main == name && sub != ""
+    end
+
+    sections
+  end
+
+  def section?(key)
+    key = Section.normalize(key)
+    @lines.has_key?(key)
+  end
+
+  private def read_config_file
+    @lines = Hash.new { |hash, key| hash[key] = [] }
+    section = Section.new([])
+
+    File.open(@path, File::RDONLY) do |file|
+      until file.eof?
+        line = parse_line(section, read_line(file))
+        section = line.section
+
+        lines_for(section).push(line)
+      end
+    end
+  rescue Errno::ENOENT
+  end
+
+  private def read_line(file)
+    buffer = ""
+
+    loop do
+      buffer.concat(file.readline)
+      return buffer unless buffer.end_with?("\\\n")
+    end
+  end
+
+  private def lines_for(section) = @lines[Section.normalize(section.name)]
+
+  private def parse_line(section, line)
+    if (match = SECTION_LINE.match(line))
+      section = Section.new([match[1], match[3]].compact)
+      Line.new(line, section)
+    elsif (match = VARIABLE_LINE.match(line))
+      variable = Variable.new(match[1], parse_value(match[2]))
+      Line.new(line, section, variable)
+    elsif BLANK_LINE.match(line)
+      Line.new(line, section, nil)
+    else
+      message = "bad config line #{line_count + 1} in file #{@path}"
+      raise ParseError, message
+    end
+  end
+
+  private def parse_value(value)
+    case value
+    when "yes", "on", "true" then true
+    when "no", "off", "false" then false
+    when INTEGER then value.to_i
+    else
+      value.gsub("\\\n", "")
+    end
+  end
+
+  private def line_count
+    @lines.each_value.reduce(0) { |n, lines| n + lines.size }
+  end
+
+  private def split_key(key)
+    key = key.map(&:to_s)
+    var = key.pop
+
+    [key, var]
+  end
+
+  private def find_lines(key, var)
+    name = Section.normalize(key)
+    return [nil, []] unless @lines.has_key?(name)
+
+    lines = @lines[name]
+    section = lines.first.section
+    normal = Variable.normalize(var)
+
+    lines = lines.select { normal == _1.normal_variable }
+    [section, lines]
+  end
+
+  private def add_variable(section, key, var, value)
+    section ||= add_section(key)
+
+    text = Variable.serialize(var, value)
+    var = Variable.new(var, value)
+    line = Line.new(text, section, var)
+
+    lines_for(section).push(line)
+  end
+
+  private def add_section(key)
+    section = Section.new(key)
+    line = Line.new(section.heading_line, section)
+
+    lines_for(section).push(line)
+    section
+  end
+
+  private def update_variable(line, var, value)
+    line.variable.value = value
+    line.text = Variable.serialize(var, value)
+  end
+
+  private def remove_all(section, lines)
+    lines.each { lines_for(section).delete(_1) }
+  end
+
+  private def remove_section(key)
+    key = Section.normalize(key)
+    @lines.delete(key) ? true : false
+  end
+end
---
+++ test/config_test.rb
@@ -0,0 +1,194 @@
+require "minitest/autorun"
+
+require "fileutils"
+require "pathname"
+
+require "config"
+
+class TestConfig < Minitest::Test
+  def open_config = Config.new(Pathname.new(@path)).tap(&:open)
+
+  def setup
+    @path = File.expand_path("../test-config", __FILE__)
+    @config = open_config
+  end
+
+  def teardown = FileUtils.rm_rf(@path)
+end
+
+class TestConfigInMemory < TestConfig
+  def setup = super
+
+  def test_return_nil_for_unknown_key
+    assert_nil(@config.get(%w[core editor]))
+  end
+
+  def test_return_value_for_known_key
+    @config.set(%w[core editor], "ed")
+    assert_equal("ed", @config.get(%w[core editor]))
+  end
+
+  def test_treat_section_names_case_insensitive
+    @config.set(%w[core editor], "ed")
+    assert_equal("ed", @config.get(%w[Core editor]))
+  end
+
+  def test_treat_variable_names_case_insensitive
+    @config.set(%w[core editor], "ed")
+    assert_equal("ed", @config.get(%w[core Editor]))
+  end
+
+  def test_retrieve_values_from_subsections
+    @config.set(%w[branch main remote], "origin")
+    assert_equal("origin", @config.get(%w[branch main remote]))
+  end
+
+  def test_treat_subsection_names_case_sensitive
+    @config.set(%w[branch main remote], "origin")
+    assert_nil(@config.get(%w[branch Main remote]))
+  end
+
+  def test_add_multiple_values_for_key
+    key = %w[remote origin fetch]
+
+    @config.add(key, "main")
+    @config.add(key, "topic")
+
+    assert_equal("topic", @config.get(key))
+    assert_equal(["main", "topic"], @config.get_all(key))
+  end
+
+  def test_refuse_setting_value_for_multi_valued_key
+    key = %w[remote origin fetch]
+
+    @config.add(key, "main")
+    @config.add(key, "topic")
+
+    assert_raises(Config::Conflict) { @config.set(key, "new-value") }
+  end
+
+  def test_replace_all_values_for_multi_valued_key
+    key = %w[remote origin fetch]
+
+    @config.add(key, "main")
+    @config.add(key, "topic")
+    @config.replace_all(key, "new-value")
+
+    assert_equal(["new-value"], @config.get_all(key))
+  end
+end
+
+class TestConfigFileStorage < TestConfig
+  def assert_file(contents) = assert_equal(contents, File.read(@path))
+
+  def setup
+    super
+
+    @config.open_for_update
+  end
+
+  def test_write_single_setting
+    @config.set(%w[core editor], "ed")
+    @config.save
+
+    assert_file <<~EOF
+      [core]
+      \teditor = ed
+    EOF
+  end
+
+  def test_write_multiple_settings
+    @config.set(%w[core editor], "ed")
+    @config.set(%w[user name], "A. U. Thor")
+    @config.set(%w[Core bare], true)
+    @config.save
+
+    assert_file <<~EOF
+      [core]
+      \teditor = ed
+      \tbare = true
+      [user]
+      \tname = A. U. Thor
+    EOF
+  end
+
+  def test_write_multiple_subsections
+    @config.set(%w[branch main remote], "origin")
+    @config.set(%w[branch Main remote], "another")
+    @config.save
+
+    assert_file <<~EOF
+      [branch "main"]
+      \tremote = origin
+      [branch "Main"]
+      \tremote = another
+    EOF
+  end
+
+  def test_write_multiple_subsections
+    @config.set(%w[branch main remote], "origin")
+    @config.set(%w[branch Main remote], "another")
+    @config.save
+
+    assert_file <<~EOF
+      [branch "main"]
+      \tremote = origin
+      [branch "Main"]
+      \tremote = another
+    EOF
+  end
+
+  def test_overwrite_variable_with_matching_name
+    @config.set(%w[merge conflictstyle], "diff3")
+    @config.set(%w[merge ConflictStyle], "none")
+    @config.save
+
+    assert_file <<~EOF
+      [merge]
+      \tConflictStyle = none
+    EOF
+  end
+
+  def test_retrieve_persisted_settings
+    @config.set(%w[core editor], "ed")
+    @config.save
+
+    assert_equal("ed", open_config.get(%w[core editor]))
+  end
+
+  def test_retrieve_variables_from_subsections
+    @config.set(%w[branch main remote], "origin")
+    @config.set(%w[branch Main remote], "another")
+    @config.save
+
+    assert_equal("origin", open_config.get(%w[branch main remote]))
+    assert_equal("another", open_config.get(%w[branch Main remote]))
+  end
+
+  def test_retrieve_variables_from_subsections_including_dots
+    @config.set(%w[url git@github.com: insteadOf], "gh:")
+    @config.save
+
+    assert_equal("gh:", open_config.get(%w[url git@github.com: insteadOf]))
+  end
+
+  def test_retain_formatting_of_existing_settings
+    @config.set(%w[core Editor], "ed")
+    @config.set(%w[user Name], "A. U. Thor")
+    @config.set(%w[core Bare], true)
+    @config.save
+
+    config = open_config
+    config.open_for_update
+    config.set(%w[Core bare], false)
+    config.save
+
+    assert_file <<~EOF
+      [core]
+      \tEditor = ed
+      \tbare = false
+      [user]
+      \tName = A. U. Thor
+    EOF
+  end
+end
