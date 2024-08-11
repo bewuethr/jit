895e21c21b5955b00cb6fe0b1d5bee965e538190
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-08-10 17:47:37 -0700

    Consider XDG base spec for config file stack
    For the global config file, Git recognizes two locations: ~/.gitconfig,
    and $XDG_CONFIG_HOME/git/config, where $XDG_CONFIG_HOME defaults to
    ~/.config.
    
    If ~/.gitconfig exists, it is used as the global config file. If it
    doesn't exist and the XDG config file exists, then the XDG file is used.
    The two files aren't combined, either one or the other is used.
    
    This change checks for the existence of the two files and sets the path
    for the global config file accordingly.

--- lib/config/stack.rb
+++ lib/config/stack.rb
@@ -4,12 +4,20 @@ require_relative "../config"
 class Config
   class Stack
     GLOBAL_CONFIG = File.expand_path("~/.gitconfig")
+    GLOBAL_CONFIG_XDG = File.expand_path("git/config",
+      ENV["XDG_CONFIG_HOME"] || File.expand_path("~/.config"))
     SYSTEM_CONFIG = "/etc/gitconfig"
 
     def initialize(git_path)
+      global_path = GLOBAL_CONFIG
+
+      if File.exist?(GLOBAL_CONFIG_XDG) && !File.exist?(GLOBAL_CONFIG)
+        global_path = GLOBAL_CONFIG_XDG
+      end
+
       @configs = {
         local: Config.new(git_path.join("config")),
-        global: Config.new(Pathname.new(GLOBAL_CONFIG)),
+        global: Config.new(Pathname.new(global_path)),
         system: Config.new(Pathname.new(SYSTEM_CONFIG))
       }
     end
