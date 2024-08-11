3c25480cbcd6103441c59188302d839a669b7d50
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-08-10 18:24:20 -0700

    Add more colours
    Add more colours by the name Git uses for them. Additional, when "fmt"
    is called with multiple parameters, the second one is considered a
    background colour, which is done by adding 10 to its SGR code.

--- lib/color.rb
+++ lib/color.rb
@@ -1,14 +1,32 @@
 module Color
   SGR_CODES = {
+    "normal" => 0,
     "bold" => 1,
+    "dim" => 2,
+    "italic" => 3,
+    "ul" => 4,
+    "reverse" => 7,
+    "strike" => 9,
+    "black" => 31,
     "red" => 31,
     "green" => 32,
     "yellow" => 33,
-    "cyan" => 36
+    "blue" => 34,
+    "magenta" => 35,
+    "cyan" => 36,
+    "white" => 37
   }
 
   def self.format(style, string)
-    codes = [*style].map { |name| SGR_CODES.fetch(name.to_s) }
+    codes = [*style].map { SGR_CODES.fetch(_1.to_s) }
+    color = false
+
+    codes.each_with_index do |code, i|
+      next unless code >= 30
+      codes[i] += 10 if color
+      color = true
+    end
+
     "\e[#{codes.join(";")}m#{string}\e[m"
   end
 end
